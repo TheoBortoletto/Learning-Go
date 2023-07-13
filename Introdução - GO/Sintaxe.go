d7c1cf6c16bab23a365632ec78f68b9e73429ab5
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-13 10:33:08 -0300

    add content

---
+++ Introdução - GO/Sintaxe.go
@@ -0,0 +1,14 @@
+/* Um arquivo 'Go' consiste nas seguintes partes:
+
+- Declaração de pacote;
+- Importar pacotes;
+- Funções;
+- Declarações e expressões
+
+Exemplo:
+*/
+
+package main // Declaração de pacotes
+import (
+	"fmt"
+) // Importar pacotes
