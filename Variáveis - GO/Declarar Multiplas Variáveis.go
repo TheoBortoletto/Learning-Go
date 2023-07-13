e46454787d6fe728dc950d1293a4d04bdbc64068
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-13 11:40:19 -0300

    add content

---
+++ Variáveis - GO/Declarar Multiplas Variáveis.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("\n\t Declaração de variáveis múltiplas: \n")
+
+	// Em 'Go', é possível declarar mútipllas variáveis na mesma linhas
+}
