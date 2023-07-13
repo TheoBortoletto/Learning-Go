6ac3c03e3c8b08aa79e78c16dd80f10cf2b163c0
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-13 11:07:27 -0300

    add content

---
+++ Introdução - GO/Comentários.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Isso é um comentário
+	fmt.Println("\nHá um comentário acima.")
+	// fmt.Println("Isso não será exibido na tela, pois é um comentário.")
+}
+
+/*Comentário multilinha*/
