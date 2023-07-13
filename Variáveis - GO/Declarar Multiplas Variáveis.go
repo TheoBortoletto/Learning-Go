24dbab818e84a4f2f999cce22d3f5a33afe64cb6
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-13 11:51:59 -0300

    add content

--- Variáveis - GO/Declarar Multiplas Variáveis.go
+++ Variáveis - GO/Declarar Multiplas Variáveis.go
@@ -5,7 +5,32 @@ import (
 )
 
 func main() {
-	fmt.Println("\n\t Declaração de variáveis múltiplas: \n")
+	fmt.Println("\n\t Declaração de variáveis múltiplas, na mesma linha: \n")
 
-	// Em 'Go', é possível declarar mútipllas variáveis na mesma linhas
+	// Em 'Go', é possível declarar mútipllas variáveis na mesma linhas:
+
+	var a, b, c, d int = 1, 3, 5, 7
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+
+	fmt.Println("\nSe o tipo de variável não for especificada, você pode declarar diferentes tipos de variáveis na mesma linha:\n")
+
+	var w, x = 6, "Olá"
+	y, z := 7, "Mundo"
+
+	fmt.Println(w)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+
+	fmt.Println("\nDeclaração de variável em um bloco: \n")
+
+	var (
+		i int
+		j int    = 1
+		k string = "hello"
+	)
 }
