93986fdf527d41cc379b8c03c39d72c8aa3b6ebb
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-14 11:56:22 -0300

    add content

---
+++ Tips de Dados- GO/Tipos Básicos.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("Tipos de dados: \n")
+
+	/*Go tem três tipos básicos de dados:
+
+	bool : representa um valor booleano e é true ou false
+	numérico : representa tipos inteiros, valores de ponto flutuante e tipos complexos
+	string : representa um valor de string*/
+
+	var a bool = true     //Booleano
+	var b int = 5         //Inteiro
+	var c float32 = 3.14  //Número flutuante
+	var d string = "Olá!" //String
+
+	fmt.Println("Booleano: ", a)
+	fmt.Println("Inteiro: ", b)
+
+}
