4916b2a1bbffc05933dc6cddedbbbe752d5b8506
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-14 12:07:44 -0300

    add content

---
+++ Tips de Dados- GO/Booleano.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("\nTipos de dados booleanos: \n")
+
+	var b1 bool = true // declaração digitada com valor inicial
+	var b2 = true      // declarção sem tipo com valor inicial
+	var b3 bool        // declaração digital sem valor inicial
+	b4 := true         // declaração sem tipo com valor inicial
+
+	fmt.Println(b1)
+	fmt.Println(b2)
+	fmt.Println(b3)
+	fmt.Println(b4)
+}
