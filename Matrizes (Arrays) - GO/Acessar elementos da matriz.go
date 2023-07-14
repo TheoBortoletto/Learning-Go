271d23e7dfd839f093c2f7e09ebd71a94f18c86f
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-14 14:52:29 -0300

    add file

---
+++ Matrizes (Arrays) - GO/Acessar elementos da matriz.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("\n\t\tAcessar elementos de uma matriz:\n")
+
+	valor := [3]int{5, 10, 15}
+
+	fmt.Println("O valor 1 é: ", valor[0], "\n")
+	fmt.Println("O valor 2 é: ", valor[1], "\n")
+	fmt.Println("O valor 3 é: ", valor[2], "\n")
+
+}
