e6a4ee6b61b2afb4832c2e14b071f6826687fa22
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-14 14:21:58 -0300

    add content

--- Tips de Dados- GO/Flutuante (float).go
+++ Tips de Dados- GO/Flutuante (float).go
@@ -17,10 +17,20 @@ func main() {
 	Dica: O tipo padrão para float é float64. Se você não especificar
 	um tipo, o tipo será float64.*/
 
+	fmt.Println("\tA palavra-chave 'float32': \n")
+
 	var x float32 = 132.78
 	var y float32 = 3.4e+38
 
 	fmt.Printf("Tipo: %T, valor: %v \n", x, x)
 	fmt.Printf("Tipo: %T, valor: %v \n\n", y, y)
+
+	fmt.Println("\tA palavra-chave 'float64':\n")
+
+	/*O float64 pode armazenar um conjunto maior de números do que float32.*/
+
+	var num float64 = 1.7e+308
+
+	fmt.Printf("Tipo: %T, valor: %v \n", num, num)
 
 }
