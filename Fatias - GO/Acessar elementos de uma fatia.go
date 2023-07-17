8b280c05c1695171edb79c7145043cc1cc0a8471
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-17 13:52:27 -0300

    add content

---
+++ Fatias - GO/Acessar elementos de uma fatia.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("\n\t\tAcessar elementos de uma fatia: \n")
+
+	valores := []int{10, 20, 30}
+
+	fmt.Println(valores[0])
+	fmt.Println(valores[2])
+}
