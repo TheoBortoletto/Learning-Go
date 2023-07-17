9fdaaa1848c046306b38c752b3070bfc287b5ab5
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-17 09:54:21 -0300

    update file

--- Matrizes (Arrays) - GO/Alterar elementos.go
+++ Matrizes (Arrays) - GO/Alterar elementos.go
@@ -11,8 +11,13 @@ func main() {
 	valores := [3]int{19.00, 12.00, 20.00}
 	var produtos = [3]string{"Leite", "Açúcar", "Presunto"}
 
+	fmt.Println("MATRIZES: \n")
+	fmt.Println(valores)
+	fmt.Println(produtos, "\n")
+
 	fmt.Println("Valor dos produtos: \n")
 	fmt.Println(produtos[0], ":", valores[0])
 	fmt.Println(produtos[1], ":", valores[1])
 	fmt.Println(produtos[2], ":", valores[2])
+
 }
