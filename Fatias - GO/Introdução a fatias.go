a6dbc4b61921f2bde2a54e4d31714654c17e8ed1
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-17 12:19:12 -0300

    add file

---
+++ Fatias - GO/Introdução a fatias.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("\n\t\tIntrodução a fatias: \n")
+
+	/* Fatias são semelhantes a matrizes, mas são mais poderosas e flexíveis.
+
+	Assim como os arrays, os slices também são usados ​​para armazenar vários valores do mesmo tipo em uma única variável.
+
+	No entanto, ao contrário das matrizes, o comprimento de uma fatia pode aumentar e diminuir conforme você desejar.
+
+	Em Go, existem várias maneiras de criar uma fatia:
+
+	Usando o formato [] tipo de dados { valores }
+	Criar uma fatia de uma matriz
+	Usando a função make() */
+
+}
