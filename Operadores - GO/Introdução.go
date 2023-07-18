05d7df79b1ceafd101204238274719fc3baeedab
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-18 11:14:07 -0300

    add content

--- Operadores - GO/Introdução.go
+++ Operadores - GO/Introdução.go
@@ -8,4 +8,11 @@ func main() {
 
 	fmt.Println("\n\t\tOperadores GO: \n")
 
+	/*Operadores são usados ​​para realizar operações em variáveis ​​e valores.
+
+	O operador '+' soma dois valores, como no exemplo abaixo:*/
+
+	var num = 15 + 25
+	fmt.Println(num, "\n")
+
 }
