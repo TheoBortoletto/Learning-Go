b9716ad1fe8101d09f6f15d5186aaddd5bc090f3
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-18 13:10:40 -0300

    add file &  content

---
+++ Operadores - GO/Operadores bit a bit.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("\n\t\tOperadores bit a bit: \n")
+
+	// Veja aqui os exemplos: https://www.w3schools.com/go/go_bitwise_operators.php
+}
