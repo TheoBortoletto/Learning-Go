43725736b717c2d9fb855d4cccfc754f1f427abc
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-18 11:17:33 -0300

    add content

--- Operadores - GO/Introdução.go
+++ Operadores - GO/Introdução.go
@@ -15,4 +15,17 @@ func main() {
 	var num = 15 + 25
 	fmt.Println(num, "\n")
 
+	/*Embora o +operador seja frequentemente usado
+	para somar dois valores, ele também pode ser
+	usado para somar uma variável e um valor, ou
+	uma variável e outra variável:*/
+
+	var (
+		sum1 = 100 + 50
+		sum2 = sum1 + 250
+		sum3 = sum2 + sum2
+	)
+
+	fmt.Println(sum3)
+
 }
