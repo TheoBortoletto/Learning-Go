85ae702bad0246588ff11baa038b439f7896da78
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-19 11:48:31 -0300

    add file

---
+++ Loop - GO/Loops aninhados.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("\n\t\tLoops aninhados: \n")
+
+	/*É possível colocar um loop dentro de outro loop.
+	Aqui, o "loop interno" será executado uma vez para cada iteração do "loop externo":*/
+
+}
