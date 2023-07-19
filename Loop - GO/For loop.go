6f284169425a71db495ab268e66ccdd06332252d
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-19 10:52:05 -0300

    add content

--- Loop - GO/For loop.go
+++ Loop - GO/For loop.go
@@ -23,4 +23,28 @@ func main() {
 	instrução3 Aumenta o valor do contador de loop.
 	*/
 
+	fmt.Println("Exemplo 1: \n")
+
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
+
+	/*Exemplo 1 explicado
+
+	i:=0; - Inicializa o contador de loop (i) e define o valor inicial para 0
+	i < 5; - Continua o loop enquanto i for menor que 5
+	i++ - Aumente o valor do contador do loop em 1 para cada iteração
+	*/
+
+	fmt.Println("\nExemmplo 2:\n")
+
+	for x := 0; x <= 100; x += 10 {
+		fmt.Println(x)
+	}
+
+	/*Exemplo 2 explicado
+	i:=0; - Inicializa o contador de loop (i) e define o valor inicial para 0
+	i <= 100; - Continua o loop enquanto i for menor ou igual a 100
+	i+=10 - Aumenta o valor do contador do loop em 10 para cada iteração
+	*/
 }
