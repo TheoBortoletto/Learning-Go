03ce06c30dc2b9f0aeb5f6adb0526f9477ae984b
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-19 15:47:05 -0300

    add content

---
+++ Funções - GO/Função 'return'.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("\n\t\tFuncão 'retur': \n")
+
+	/*Se você deseja que a função retorne um valor,
+	você precisa definir o tipo de dados do valor de
+	retorno (como int, string, etc) e também usar a
+	returnpalavra-chave dentro da função:*/
+}
