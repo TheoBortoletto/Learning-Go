dc4447827e6337520697eb496d5134a78f0fea01
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-19 12:15:00 -0300

    add file

--- Loop - GO/A palavra-chave 'range'.go
+++ Loop - GO/A palavra-chave 'range'.go
@@ -8,4 +8,17 @@ func main() {
 
 	fmt.Println("\n\t\tA palavra-chave 'range': \n")
 
+	/*A palavra-chave 'range' é usada para iterar mais
+		facilmente em uma matriz, fatia ou mapa. Ele retorna o
+		índice e o valor.
+
+		A palavra-chave 'range' é usada assim:
+
+		SINTAXE:
+
+		for index, value := array|slice|map {
+	    code to be executed for each iteration
+	}
+
+	*/
 }
