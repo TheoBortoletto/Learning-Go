3d523ef65f059288747e3aacec7c50beeb2e2f89
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-19 12:01:38 -0300

    add file & content

--- Loop - GO/Loops aninhados.go
+++ Loop - GO/Loops aninhados.go
@@ -11,4 +11,12 @@ func main() {
 	/*É possível colocar um loop dentro de outro loop.
 	Aqui, o "loop interno" será executado uma vez para cada iteração do "loop externo":*/
 
+	adjetivos := [2]string{"Grande", "Bom"}
+	frutas := [3]string{"Maçã", "Banana", "Laranja"}
+
+	for i := 0; i < len(adjetivos); i++ {
+		for j := 0; j < len(frutas); j++ {
+			fmt.Println(adjetivos[i], frutas[j])
+		}
+	}
 }
