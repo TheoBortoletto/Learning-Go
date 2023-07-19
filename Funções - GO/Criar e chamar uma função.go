e24402a77ec4961d60a26f84a00529c54362e559
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-19 15:12:41 -0300

    add file & content

---
+++ Funções - GO/Criar e chamar uma função.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("\n\t\tCriar e chamar uma função: \n")
+
+	/*SINTAXE:
+
+	func nomeDaFunção() {
+		código a ser executado
+	}*/
+}
