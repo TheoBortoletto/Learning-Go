833cbb63cce1ce63e94cbc762ec747e947752473
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-19 10:32:08 -0300

    add content

---
+++ Loop - GO/For loop.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("\n\t\tLoop 'for': \n")
+
+	/*SINTAXE:
+
+	for instrução1; instrução2; instrução3 {
+	   	código a ser executado em cada iteração
+	}
+
+	instrução1 Inicializa o valor do contador de loop.
+
+	instrução2 Avaliado para cada iteração do loop.
+	Se for avaliado como TRUE, o loop continua.
+	Se for avaliado como FALSE, o loop termina.
+
+	instrução3 Aumenta o valor do contador de loop.
+	*/
+
+}
