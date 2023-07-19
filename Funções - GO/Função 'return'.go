20e64411029c2e968a8133b0cc316536bb2a2520
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-19 15:51:55 -0300

    add content

--- Funções - GO/Função 'return'.go
+++ Funções - GO/Função 'return'.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 )
 
+func myFunciton(x int, y int) int {
+	return x + y
+}
+
 func main() {
 
 	fmt.Println("\n\t\tFuncão 'retur': \n")
@@ -11,5 +15,15 @@ func main() {
 	/*Se você deseja que a função retorne um valor,
 	você precisa definir o tipo de dados do valor de
 	retorno (como int, string, etc) e também usar a
-	returnpalavra-chave dentro da função:*/
+	returnpalavra-chave dentro da função:
+
+	SINTAXE
+
+	func nomeFunção (param1 tipo, param2 tipo) tipo {
+		código a ser executado
+		return output
+	}*/
+
+	fmt.Println(myFunciton(1, 2))
+
 }
