d86e48b203759bc81fb2c20135c17bf455ba70bb
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-20 15:40:11 -0300

    add content

---
+++ Estruturas (Structs) - GO/Passar struct como argumento de função.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type Pessoa struct {
+	nome string
+	idade int
+	cargo string
+	salario int
+}
+
+func main() {
+
+	fmt.Println("\n\t\tPassar sctruct como argumento de função: \n")
+
+	var
+
+}
