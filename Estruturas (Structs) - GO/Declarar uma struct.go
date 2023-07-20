7861c2a12d239fd5f84b4e3b85d258256f61ae1b
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-20 15:15:14 -0300

    add file

--- Estruturas (Structs) - GO/Declarar uma struct.go
+++ Estruturas (Structs) - GO/Declarar uma struct.go
@@ -4,5 +4,16 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("\n\t\tEstruturas (Structs):")
+	fmt.Println("\n\t\tEstruturas (Structs):\n")
+
+	/*Um struct (abreviação de structure) é usado para criar uma
+	coleção de membros de diferentes tipos de dados em uma única variável.
+
+	Enquanto arrays são usados ​​para armazenar vários valores
+	do mesmo tipo de dados em uma única variável, structs são
+	usados ​​para armazenar vários valores de diferentes tipos
+	de dados em uma única variável.
+
+	Uma struct pode ser útil para agrupar dados para criar registros.*/
+
 }
