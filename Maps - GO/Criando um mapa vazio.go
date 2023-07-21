87844980f5543965d692875f33815cdb9e6bb435
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-21 12:07:20 -0300

    add content

--- Maps - GO/Criando um mapa vazio.go
+++ Maps - GO/Criando um mapa vazio.go
@@ -6,4 +6,22 @@ func main() {
 
 	fmt.Println("\n\t\tCriando um mapa vazio:\n")
 
+	/*Existem duas maneiras de criar um mapa vazio. Uma é
+	usando a função make() e a outra é usando a seguinte sintaxe:
+
+	var a map[TipoChave]TipoValor
+
+	OBS: A função make() é o caminho certo para
+	criar um mapa vazio. Se você criar um mapa
+	vazio de uma maneira diferente e escrever
+	nele, isso causará um pânico no tempo de
+	execução.
+	*/
+
+	var a = make(map[string]string)
+	var b map[string]string
+
+	fmt.Println(a == nil) // -> EM GO, 'nil' é um valor nulo para ponteiros, maps, slices etc...
+	fmt.Println(b == nil)
+
 }
