88fc7f006e3082383d22782ecdc208817dac9f61
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-21 12:20:51 -0300

    add file & content

---
+++ Maps - GO/Tipos de valores e chaves permitidos.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("\n\t\tTipos de chaves e valores permitidos: \n")
+
+	/*Tipos de chave permitidos:
+
+	A chave do mapa pode ser de qualquer tipo de dados
+	para o qual o operador de igualdade ( == ) é definido. Esses incluem:
+
+	- Booleanos
+	- Números
+	- Cordas
+	- Matrizes
+	- Ponteiros
+	- Estruturas
+	- Interfaces (desde que o tipo dinâmico suporte igualdade)
+
+	Os tipos de chave inválidos são:
+
+	- Fatias
+	- Mapas
+	- Funções
+
+	Esses tipos são inválidos porque o operador de igualdade ( == ) não está definido para eles.
+
+	Tipos de valor permitidos:
+
+	Os valores do mapa podem ser de qualquer tipo.*/
+}
