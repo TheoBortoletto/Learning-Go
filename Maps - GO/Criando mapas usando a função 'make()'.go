6ad74fd5200252ecd69cb429547e97ec791baa37
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-21 11:05:16 -0300

    add content

---
+++ Maps - GO/Criando mapas usando a função 'make()'.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("\n\t\tCriando mapas usando a função 'make()': \n")
+
+	/*SINTAXE:
+
+	var a = make(map[TipoChave]TipoValor)
+	b := make(map[TipoChave]TipoValor)
+	*/
+
+}
