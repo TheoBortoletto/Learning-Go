3be7936f5c8c5bd5a425acf1271c338b6a4a741e
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-21 10:24:04 -0300

    add content

--- Maps - GO/Criando mapas.go
+++ Maps - GO/Criando mapas.go
@@ -4,5 +4,30 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("")
+	fmt.Println("\n\t\tCriando Mapas:\n")
+
+	/*Os mapas são usados ​​para armazenar valores de dados em pares chave:valor.
+
+	Cada elemento em um mapa é um par chave:valor.
+
+	Um mapa é uma coleção não ordenada e mutável que não permite duplicatas.
+
+	O comprimento de um mapa é o número de seus elementos. Você pode encontrá-lo usando a len()função.
+
+	O valor padrão de um mapa é nulo.
+
+	Os mapas contêm referências a uma tabela de hash subjacente.
+
+	Go tem várias maneiras de criar mapas.*/
+
+	/*SINTAXE:
+	
+	var a = map[TipoChave]TipoValor{chave1:valor1, chave2:valor2,...}
+
+	b := map[TipoChave]TipoValor{chave1:valor1, chave2:valor2...}
+	*/
+
+	var a = map[string]string{"Marca:" "Ford", "Modelo:" "Mustang"}
+
+	
 }
