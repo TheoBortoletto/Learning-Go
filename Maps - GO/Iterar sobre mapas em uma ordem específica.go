d92ed08fce4762729fe911a73a1eb68823bd7902
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-07-24 13:56:13 -0300

    add file

--- Maps - GO/Iterar sobre mapas em uma ordem específica.go
+++ Maps - GO/Iterar sobre mapas em uma ordem específica.go
@@ -1,1 +1,29 @@
 package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("\n\t\tIterar sobre mapas em uma ordem específica: \n")
+
+	/*Os mapas são estruturas de dados não ordenadas.
+	Se você precisar iterar em um mapa em uma ordem específica,
+	deverá ter uma estrutura de dados separada que especifique essa ordem.*/
+
+	a := map[string]int{"one": 1, "two": 2, "three": 3, "four": 4}
+
+	var b []string // defining the order
+	b = append(b, "one", "two", "three", "four")
+
+	for k, v := range a { // loop with no order
+		fmt.Printf("%v : %v, ", k, v)
+	}
+
+	fmt.Println()
+
+	for _, element := range b { // loop with the defined order
+		fmt.Printf("%v : %v, ", element, a[element])
+	}
+
+	fmt.Println()
+}
